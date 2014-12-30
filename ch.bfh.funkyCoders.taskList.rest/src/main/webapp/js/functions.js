c29eab248de40b76f51865f707541adbca7a6551
f4nky <fankm9@bfh.ch>
2014-12-30 17:03:48 +0100

    few modifications

--- ch.bfh.funkyCoders.taskList.rest/src/main/webapp/js/functions.js
+++ ch.bfh.funkyCoders.taskList.rest/src/main/webapp/js/functions.js
@@ -2,6 +2,14 @@ $(document).ready(function() {
 	toggleNavSidebar();
 });
 
+$(window).on('resize', function() {
+	if ($(document).outerWidth() >= 768) {
+		if ($('#wrapper').hasClass('toggled')) {
+			$('#wrapper').removeClass('toggled');
+		}
+	}
+});
+
 /* === Toggle navigation sidebar === */
 function toggleNavSidebar() {
 	$('.navbar-toggle').click(function() {
